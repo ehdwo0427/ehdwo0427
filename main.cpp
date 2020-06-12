c107201f06461307c660245bf9ca6b271d886fed
이동재 <dong@idongjaeui-MacBookPro.local>
2020-06-12 19:14:14 +0900

    재귀함수 하노이

--- main.cpp
+++ main.cpp
@@ -1,46 +1,26 @@
 #include<iostream>
-#include<algorithm>
-#define Max 1000000000
+#include<stdio.h>
+
 using namespace std;
-int N, A, B, C, D, high = -Max, low = Max;
-int Array[12];
-void input(){
-    cin >> N;
-    for(int i = 0; i < N; i++){
-        cin >> Array[i];
-    }
-    cin >> A >> B >> C >> D;
-}
 
-void solution(int plus, int minus, int multiply, int divide, int cnt, int sum){
-    if(cnt == N){
-        high = max(sum, high);
-        low  = min(sum, low);
-        return;
+
+void hinoi(int n, int a, int b, int c){
+    if(n == 1){
+        printf("%d %d\n", a ,b );
     }
-    
-    if(plus > 0){
-        solution(plus - 1, minus, multiply, divide, cnt + 1, sum + Array[cnt]);
-    }
-    if(minus > 0){
-        solution(plus, minus - 1, multiply, divide, cnt + 1, sum - Array[cnt]);
-    }
-    if(multiply > 0){
-        solution(plus, minus, multiply - 1, divide, cnt + 1, sum * Array[cnt]);
-    }
-    if(divide > 0){
-        solution(plus, minus, multiply, divide - 1, cnt + 1, sum / Array[cnt]);
+    else{
+        hinoi(n-1, a, c, b);
+        printf("%d %d\n", a, c);
+        hinoi(n-1, c, a, b);
     }
 }
 
-void solve(){
-    input();
-    solution(A,B,C,D,1,Array[0]);
-    cout<< high << '\n' << low << '\n';
-}
+
+
+
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL); cout.tie(NULL);
-    solve();
+    int n=3,a=1,b=2,c=3;
+    hinoi(n,a,b,c);
+    
     return 0;
 }
